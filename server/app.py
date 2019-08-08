022150621a071b1176a45d0c0a0a581b62084fd2
Arianna Pintér <arianna.pinter@shiwaforce.com>
2019-08-08 16:21:20 +0200

    added books endpoint

--- server/app.py
+++ server/app.py
@@ -4,6 +4,24 @@ from flask_cors import CORS
 # CORS (Cross Origin Resource Sharing) is needed to make cross-origin requests
 # in production environment, only allow cross origin requests from trusted domains 
 
+BOOKS = [
+    {
+        'author': 'The Shining',
+        'title': 'Stephen King',
+        'read': True
+    },
+    {
+        'author': 'J. K. Rowling',
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'read': True
+    },
+    {
+        'author': 'On the Road',
+        'title': 'Jack Kerouac',
+        'read': False
+    }
+]
+
 # configuration
 DEBUG = True
 
@@ -14,12 +32,20 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+
 
 if __name__ == '__main__':
     app.run()
+
+
